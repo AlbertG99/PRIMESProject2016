1785fb72c2194054a402fa1134be013d90cfe07f
Alik (School Laptop) <Albert@Alberts-MacBook-Air.local>
2016-03-04 10:56:50 -0500

    Started to Implement Warping Error
    - Runs out of heap space

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,44 @@
+import trainableSegmentation.metrics.WarpingError;
+import ij.IJ;
+import ij.ImagePlus;
+
 public class Main {
 	public static void main(String[] args) throws Exception {
-		Tiff tiff1 = new Tiff ("/Users/Albert/Dropbox/Google Drive/Boyden PRIMES/Example/Uygar.TIF");
-		Tiff tiff2 = new Tiff ("/Users/Albert/Dropbox/Google Drive/Boyden PRIMES/Example/Bandy.TIF");
-		checkCompatibility(tiff1, tiff2);
+		//		Tiff tiff1 = new Tiff ("/Users/Albert/Dropbox/Google Drive/Boyden PRIMES/Example/Uygar.TIF");
+		//		Tiff tiff2 = new Tiff ("/Users/Albert/Dropbox/Google Drive/Boyden PRIMES/Example/Bandy.TIF");
+		//		checkCompatibility(tiff1, tiff2);
 		
+		System.gc();
+		warpingError();
+	}
+
+	public static void warpingError () { //Source: http://fiji.sc/Topology_preserving_warping_error
+		// original labels
+		ImagePlus originalLabels = IJ.openImage("/Users/Albert/Dropbox/Google Drive/Boyden PRIMES/Example/Uygar.TIF");
+
+		// proposed (new) labels
+		ImagePlus proposedLabels = IJ.openImage("/Users/Albert/Dropbox/Google Drive/Boyden PRIMES/Example/Bandy.TIF");
+		
+		// mask with geometric constraints
+		ImagePlus mask = IJ.openImage("/Users/Albert/Dropbox/Google Drive/Boyden PRIMES/Example/Bandy.TIF");
+
+		// threshold to binarize labels (just in case they are not binary)
+		double threshold = 0.5;
+
+		WarpingError metric = new WarpingError( originalLabels, proposedLabels, mask );
+		
+		System.gc();
+		double warpingError = metric.getMetricValue( threshold );
+
+		IJ.log("Warping error between source image " + originalLabels.getTitle() + " and target image " + proposedLabels.getTitle() + " = " + warpingError);
+	}
+
+	public static byte[][] pixelError (Tiff tiff1, Tiff tiff2) throws Exception {
 		int numPages = tiff1.getNumPages();
 		int numPixels = tiff1.getPixels(0).length;
+
 		byte[][] fullOutput = new byte[numPages][numPixels];
-		
+
 		for (int page = 0; page < numPages; page++) {
 			System.out.println(page);
 			try {
@@ -18,10 +49,10 @@ public class Main {
 				System.gc();
 			}
 		}
-		
-		new Tiff(fullOutput);
+
+		return fullOutput;
 	}
-	
+
 	public static byte[] comparePage (byte[] page1, byte[] page2) {
 		/*
 		0 = neither
@@ -31,10 +62,10 @@ public class Main {
 		 */
 		int numPixels = page1.length;
 		byte[] comparison = new byte[numPixels];
-		
+
 		int page1Pix;
 		int page2Pix;
-		
+
 		for (int i = 0; i < numPixels; i++) {
 			page1Pix = page1[i];
 			page2Pix = page2[i];
@@ -51,10 +82,10 @@ public class Main {
 				comparison[i] = 0;
 			}
 		}
-		
+
 		return comparison;
 	}
-	
+
 	public static void checkCompatibility (Tiff tiff1, Tiff tiff2) throws Exception { // Checks if two TIFF files are compatible and can be further worked with
 		// Check if number of pages matches
 		int numPages = tiff1.getNumPages();
